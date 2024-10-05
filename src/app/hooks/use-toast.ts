641cee56faec6d10cb7ed9f25710340257605b69
Taylan Tatlı <taylantatli90@gmail.com>
2024-10-05 23:54:38 +0300

    refactor: Update use-toast.ts exports order

--- src/app/hooks/use-toast.ts
+++ src/app/hooks/use-toast.ts
@@ -17,7 +17,7 @@ type ToasterToast = ToastProps & {
   description?: React.ReactNode
   action?: ToastActionElement
 }
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
@@ -191,4 +191,5 @@ function useToast() {
   }
 }
 
-export { useToast, toast }
+export { toast, useToast }
+
