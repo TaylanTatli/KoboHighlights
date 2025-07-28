31c0ab19910ea2f333bd1827ab6eaddb6c325034
TaylanTatli <taylantatli90@gmail.com>
2025-07-28 03:20:44 +0300

    feat: enhance file upload component with additional file type support

--- src/app/components/FileUpload.tsx
+++ src/app/components/FileUpload.tsx
@@ -14,7 +14,12 @@ const FileUpload: React.FC<FileUploadProps> = ({
   const [showDialog, setShowDialog] = useState(false);
   const [filesToUpload, setFilesToUpload] = useState<File[]>([]);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    accept: { "application/x-sqlite3": [".sqlite"] },
+    accept: {
+      "application/x-sqlite3": [".sqlite"],
+      "application/vnd.sqlite3": [".sqlite"],
+      "application/octet-stream": [".sqlite"],
+      "*/*": [".sqlite"],
+    },
     onDrop: (acceptedFiles) => {
       if (isDatabaseLoaded) {
         setFilesToUpload(acceptedFiles);
@@ -36,45 +41,45 @@ const FileUpload: React.FC<FileUploadProps> = ({
     <div>
       <div
         {...getRootProps()}
-        className={`h-fill group relative grid w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed border-muted-foreground/25 text-center transition hover:bg-muted/50 dark:hover:bg-white/5 ${
+        className={`h-fill group border-muted-foreground/25 hover:bg-muted/50 relative grid w-full cursor-pointer place-items-center rounded-lg border-2 border-dashed text-center transition dark:hover:bg-white/5 ${
           isDragActive ? "border-primary bg-muted/50 dark:bg-white/5" : ""
         } ${isMobile ? "p-2" : "p-6"}`}
       >
         <input {...getInputProps()} />
-        <div className="flex flex-col items-center justify-center gap-2 text-muted-foreground">
+        <div className="text-muted-foreground flex flex-col items-center justify-center gap-2">
           {isMobile ? (
             <div className="flex flex-row items-center gap-2 px-5">
               <div className="p-0">
                 <Upload
-                  className="size-5 text-muted-foreground"
+                  className="text-muted-foreground size-5"
                   aria-hidden="true"
                 />
               </div>
-              <p className="font-medium text-muted-foreground">
+              <p className="text-muted-foreground font-medium">
                 {t("drop_here_mobile")}
               </p>
             </div>
           ) : isDragActive ? (
             <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-              <div className="rounded-full border border-dashed border-muted-foreground/25 p-3">
+              <div className="border-muted-foreground/25 rounded-full border border-dashed p-3">
                 <Upload
-                  className="size-5 text-muted-foreground"
+                  className="text-muted-foreground size-5"
                   aria-hidden="true"
                 />
               </div>
-              <p className="font-medium text-muted-foreground">
+              <p className="text-muted-foreground font-medium">
                 {t("drop_here")}
               </p>
             </div>
           ) : (
             <div className="flex flex-col items-center justify-center gap-4 sm:px-5">
-              <div className="rounded-full border border-dashed border-muted-foreground/25 p-3">
+              <div className="border-muted-foreground/25 rounded-full border border-dashed p-3">
                 <Upload
-                  className="size-5 text-muted-foreground"
+                  className="text-muted-foreground size-5"
                   aria-hidden="true"
                 />
               </div>
-              <p className="font-medium text-muted-foreground">
+              <p className="text-muted-foreground font-medium">
                 {t.rich("drag_drop_select", {
                   Badge: (chunks) => <Badge>{chunks}</Badge>,
                 })}
