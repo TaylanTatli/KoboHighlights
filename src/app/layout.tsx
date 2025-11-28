360e17b763bcf742598662870c9d7f626878f234
Taylan Tatlı <taylantatli90@gmail.com>
2025-11-28 21:57:28 +0300

    feat: update layout and manifest for enhanced metadata and icon support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,28 @@ import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Hylit",
+  title: "Hylit | Kobo, Kindle & KoReader Highlight Viewer and Manager",
   description:
     "Hylit (previously KoboHighlights) is a web application designed to extract and display highlights from the Kobo (KoboReader.sqlite) and Kindle/KoReader (My Clippings.txt).",
+  manifest: '/manifest.json',
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.ico",
-    apple: "/apple-icon.png",
-  },
+    icon: [
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: {
+      url: '/apple-icon.png',
+      sizes: '180x180',
+    },
+    other: [
+        {
+          rel: 'icon',
+          url: '/android-icon.png',
+          sizes: '192x192',
+        },
+    ]
+  }
 };
 
 export default async function RootLayout({
